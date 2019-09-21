3ab6bc731c9b7be49f540760ddcfbe58ae88b4a2
Yasuaki KAWAKAMI <kawakami@georepublic.co.jp>
2019-09-21 10:52:26 +0900

    add map

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,8 @@ export default {
   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    'nuxt-leaflet'
   ],
   /*
   ** Axios module configuration
@@ -59,8 +60,13 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
+        light: {
+          primary: colors.red.darken1,
+          secondary: colors.red.lighten4,
+          accent: colors.indigo.base
+        },
         dark: {
           primary: colors.blue.darken2,
           accent: colors.grey.darken3,
